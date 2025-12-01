2c6b839cdea4503a9eac9883d2208c074c76049d
mario-pc <mariopersonalweb@gmail.com>
2025-12-01 14:12:22 +0100

    2nd try: wrong

--- python/1/dial.py
+++ python/1/dial.py
@@ -4,11 +4,17 @@ arrow = 50
 password = 0
 rotations = []
 
-def move_left(arrow, n):
-    return arrow - n
-    
-def move_right(arrow, n):
-    return arrow + n
+def move_arrow(arrow, direction, n):
+
+    new_arrow = arrow + (n if direction == 'right' else -n)
+
+    # To make sure the arrow loops back and stays between 0 and 99 
+    if new_arrow >= 100:
+        return new_arrow - 100
+    elif new_arrow < 0:
+        return new_arrow + 100
+    else:
+        return new_arrow
 
 with open("rotations.txt", "r") as file:
     for line in file:
@@ -20,12 +26,12 @@ for rotation in rotations:
     n_clicks = int(rotation[1:])
 
     if rotation[0] == 'L':
-        new_arrow = move_left(arrow, n_clicks)
+        new_arrow = move_arrow(arrow, 'left', n_clicks)
 
     elif rotation[0] == 'R':
-        new_arrow = move_right(arrow, n_clicks)
+        new_arrow = move_arrow(arrow, 'right', n_clicks)
 
-    if (new_arrow%50 == 0) or (new_arrow == 0):
+    if new_arrow == 0:
         password += 1
 
     arrow = new_arrow
